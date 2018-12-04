f12c314d4fd62996cb13c5ac4d8be105b442c103
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2018-12-04 15:07:17 +0100

    horizontal movement script

---
+++ dot_net_unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody playerRb;
+    public float speed;
+    private float horizontal; 
+
+	void Start () {
+        playerRb = GetComponent<Rigidbody>();
+	}
+	
+	
+	void Update () {
+        horizontal = Input.GetAxis("Horizontal");
+        playerRb.AddForce(Vector3.right * speed * horizontal);
+	}
+}
